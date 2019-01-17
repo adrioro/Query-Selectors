40ba3210c0331258bfc663b0dbbeb22dc3747e61
adrioro <artistsvillage@me.com>
2019-01-17 13:52:21 -0500

    Eighth commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-var element = document.querySelector("nav ul li a");
+/* Inspector
+element
+<a href=​"#">​Home​</a>​
+element.innerText = "New Home"
+"New Home"
+*/
+
+//var myElement = document.querySelector("nav ul li a"); // Selects one item.
+var navigation = document.querySelectorAll('nav ul li a'); //Selects all the items.
+
+//Dont use globals. Work inside objects instead. This is just for practice purposes.
+
+
+
+if (navigation.length) {
+    for (item in navigation) {
+        var i = navigation[item];
+        console.log(i.text + " - " + i.data('href'));
+
+    }
+}
